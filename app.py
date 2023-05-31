1c11331a1af8b1e624623fe9cf3a80b222ac9836
Sergen1993 <14488@coderacademy.edu.au>
2023-05-31 12:11:41 +1000

    WIP

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
+
+db = SQLAlchemy(app)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    date_created = db.Column(db.Date())
+
+db.create_all()
+
+
+app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('tables created successfully')
 
 @app.route('/')
 def index():
