a72c8bc34e5634a40e618eb79ae4a080180bb549
Sergen1993 <14488@coderacademy.edu.au>
2023-06-05 16:38:35 +1000

    User model added

--- app.py
+++ app.py
@@ -14,6 +14,19 @@ app.config[
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class User(db.Model):
+   __tablename__ = 'users'
+
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String)
+   email = db.Column(db.String, nullable=False, unique=True)
+   password = db.Column(db.String, nullable=False)
+   is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+   class Meta:
+      fields = ('name', 'email', 'is_admin')
+
 class Card(db.Model):
     __tablename__ = "cards"
 
