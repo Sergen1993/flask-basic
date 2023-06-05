816257e157bf35983afdff2a9f7a9b69f3ec8eb3
Sergen1993 <14488@coderacademy.edu.au>
2023-06-05 16:01:04 +1000

    Flask installed

--- app.py
+++ app.py
@@ -7,79 +7,81 @@ app = Flask(__name__)
 
 app.config['JSON_SORT_KEYS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 class Card(db.Model):
-  __tablename__ = 'cards'
+    __tablename__ = "cards"
 
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(100))
-  description = db.Column(db.Text())
-  status = db.Column(db.String(30))
-  date_created = db.Column(db.Date())
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    status = db.Column(db.String(30))
+    date_created = db.Column(db.Date())
 
 class CardSchema(ma.Schema):
-   class Meta:
-      fields = ('id', 'title', 'description', 'status', 'date_created')
-      ordered = True
+  class Meta:
+    fields = ('id', 'title', 'description', 'status')
+    ordered = True
 
-
-@app.cli.command('create')
+@app.cli.command("create")
 def create_db():
-  db.drop_all()
-  db.create_all()
-  print('Tables created successfully')
+    db.drop_all()
+    db.create_all()
+    print("Tables created successfully")
 
-@app.cli.command('seed')
+
+@app.cli.command("seed")
 def seed_db():
-    # Create instances of the Card model in memory
+    # Create an instance of the Card model in memory
     cards = [
         Card(
-            title='Start the project',
-            description='Stage 1 - Create an ERD',
+            title="Start the project",
+            description="Stage 1 - Create an ERD",
             status="Done",
-            date_created=date.today()
+            date_created=date.today(),
         ),
         Card(
-            title='ORM Queries',
-            description='Stage 2 - Implement several queries',
+            title="ORM Queries",
+            description="Stage 2 - Implement several queries",
             status="In Progress",
-            date_created=date.today()
+            date_created=date.today(),
         ),
         Card(
-            title='Marshmallow',
-            description='Stage 3 - Implement jsonify of models',
-            status="In progress",
-            date_created=date.today()
-        )
+            title="Marshmallow",
+            description="Stage 3 - Implement jsonify of models",
+            status="In Progress",
+            date_created=date.today(),
+        ),
     ]
 
     # Truncate the Card table
     db.session.query(Card).delete()
 
-    # Add each card to the session (transaction)
-    for card in cards:
-        db.session.add(card)
+    # Add the card to the session (transaction)
+    db.session.add_all(cards)
 
     # Commit the transaction to the database
     db.session.commit()
-    print('Models seeded')
+    print("Models seeded")
 
 
 @app.route('/cards')
 def all_cards():
-    # Select all cards from the table
-    stmt = db.select(Card).order_by (Card.status.desc())
-    cards = db.session.scalars(stmt).all()
-    return CardSchema(many=True).dump(cards)
+  # select * from cards;
+  stmt = db.select(Card).order_by(Card.status.desc())
+  cards = db.session.scalars(stmt).all()
+  return CardSchema(many=True).dump(cards)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-  return 'Hello World!'
+    return "Hello World!"
 
-if __name__ == '__main__':
-  app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
