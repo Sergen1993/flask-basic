c6547c336603cc9ddcf8a1e25141d599efa5fd83
Sergen1993 <14488@coderacademy.edu.au>
2023-06-12 04:25:59 +1000

    Created user model and added .env

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
@@ -6,40 +6,29 @@ from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 from datetime import timedelta
+from os import environ
+from dotenv import load_dotenv
+from models.user import User, UserSchema
+from inti import db, ma, bcyrpt, jwt
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = 'Ministry of Silly Walks'
-
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello"
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-bcrypt = Bcrypt(app)
-jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
 
 def admin_required():
     user_email = get_jwt_identity()
     stmt = db.select(User).filter_by(email=user_email)
     user = db.session.scalar(stmt)
     if not (user and user.is_admin):
-        return {'error': 'You must be an admin'}, 401
+        abort(401)
 
+@app.errorhandler(401)
+def unauthorized(err):
+    return {'error': 'You must be an admin'}, 401
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('name', 'email', 'password', 'is_admin')
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -136,9 +125,7 @@ def login():
 @jwt_required()
 def all_cards():
     admin_required()
-    stmt = db.select(Card).order_by(Card.status.desc())
-    cards = db.session.scalars(stmt).all()
-    return CardSchema(many=True).dump(cards)
+
 
 @app.route("/")
 def index():
